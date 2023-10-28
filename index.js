06a0a608413251c84cebf42e674c103fb9fedec0
whps40333 <whps40333@gmail.com>
2023-10-28 21:39:27 +0800

    finish day_10 pratice

---
+++ index.js
@@ -0,0 +1,118 @@
+const checkboxes = Array.from(
+  document.querySelectorAll('.inbox input[type="checkbox"]')
+);
+
+// const checkboxes = Array.from(
+//   document.querySelectorAll('.inbox input[type="checkbox"]')
+// )
+
+let checkEle = null;
+// let checkEle = null;
+
+function handleCheck(e) {
+  // e is MouseEvent
+  //console.log(e);
+  const self = this;
+  if (e.shiftKey && e.target.checked) {
+    //console.info("this is shift & checked");
+    let isBetween = false;
+    checkboxes.map((ele) => {
+      if ((self === ele || checkEle === ele) && checkEle && checkEle !== self) {
+        //   console.info("slef equal ele");
+        //   console.dir(ele);
+        isBetween = !isBetween;
+      }
+      if (isBetween) ele.checked = true;
+    });
+  }
+  checkEle = self.checked ? self : null;
+}
+
+// // 按住shift鍵
+// function handleCheck(e){
+//   const self = this;
+//   if(e.shiftKey && e.target.checked){
+//     let isBetween = false;
+//     checkboxes.map(
+//       ele=>{
+//         if((self === ele || checkEle === ele) && checkEle && checkEle !== self){
+//           isBetween = !isBetween;
+//         }
+//         if(isBetween) ele.checked = true;
+//       }
+//     )
+//   }
+//   checkEle = self.checked ? self :null;
+// }
+/**
+ * 延伸部分
+ *
+ * @param {any} e
+ */
+function extenCheck(e) {
+  const self = this;
+  if (!e.target.checked) return;
+
+  // 目前的選取元素
+  const nowSelIndex = checkboxes.findIndex((ele) => ele === self);
+
+  if (!e.shiftKey) return;
+
+  const nearMinCheckboxIndex = checkboxes.findIndex(
+    (ele, index) =>
+      index !== nowSelIndex && index < nowSelIndex && ele.checked === true
+  );
+  const nearMaxCheckboxIndex = checkboxes.findIndex(
+    (ele, index) =>
+      index !== nowSelIndex && index > nowSelIndex && ele.checked === true
+  );
+
+  if (nearMaxCheckboxIndex !== -1) {
+    setCheckbox(nowSelIndex, nearMaxCheckboxIndex);
+  }
+
+  if (nearMinCheckboxIndex !== -1) {
+    setCheckbox(nearMinCheckboxIndex, nowSelIndex);
+  }
+}
+
+// function extenCheck(e){
+//   const self = this;
+//   if(!e.target.checked)return;
+
+//   const nowSelIndex = checkboxes.findIndex(ele => ele === self);
+
+//   if(!e.shiftKey)return;
+
+//   const nearMinCheckboxIndex = checkboxes.findIndex(
+//     (ele,index)=>
+//     index !== nowSelIndex && index < nowSelIndex && ele.checked === true
+//   );
+
+//   const nearMaxCheckboxIndex = checkboxes.findIndex(
+//     (ele,index)=>
+//     index !== nowSelIndex && index > nowSelIndex && ele.checked === true
+//   );
+
+//   if(nearMaxCheckboxIndex !== -1){
+//     setCheckbox(nowSelIndex, nearMaxCheckboxIndex);
+//   }
+
+//   if(nearMinCheckboxIndex !== -1){
+//     setCheckbox(nearMinCheckboxIndex,nowSelIndex);
+//   }
+// }
+
+/**
+ * 設定 checkboxes 為選取
+ *
+ * @param {any} initIndex 索引初始
+ * @param {any} aryLength 迴圈長度
+ */
+function setCheckbox(initIndex, aryLength) {
+  for (let index = initIndex; index < aryLength; index++) {
+    checkboxes[index].checked = true;
+  }
+}
+
+checkboxes.map((ele) => ele.addEventListener("click", extenCheck));
